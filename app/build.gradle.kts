d265c8c8f51912edb18117152210590d6aede194
Lei <134989539+LeiSteins@users.noreply.github.com>
2026-07-17 01:00:55 +0800

    chore: 发布 0.0.9 版本
    - 更新 Android versionCode 至 9
    
    - 更新 Android versionName 至 0.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "top.steins.autologin"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
