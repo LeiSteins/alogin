077ff9e00f06cf584ec0b943c407adb377491eed
Lei <134989539+LeiSteins@users.noreply.github.com>
2026-09-13 17:59:14 +0800

    chore: 发布0.1.7版本
    - 更新versionCode至17
    
    - 更新versionName至0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "top.steins.autologin"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.1.6"
+        versionCode = 17
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
